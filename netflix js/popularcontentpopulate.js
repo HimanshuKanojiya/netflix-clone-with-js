508430c5b74cdaab55619d2329d419c6d61380ef
Himanshu Kanojiya <himanshukanojiya825@gmail.com>
2021-06-15 22:14:40 +0530

    Update popularcontentpopulate.js

--- netflix js/popularcontentpopulate.js
+++ netflix js/popularcontentpopulate.js
@@ -1,13 +1,14 @@
 //This script is used in all pages
 
-function createimagearray(title, image, movieid){
+const createimagearray = (title, image, movieid) => {
     //This function will create image HTML using
     //Templating and placeholder
     return `
     <img src="${image}" alt="${title}" onclick="showcasetrailer(${movieid}, window.pageYOffset)">
     `
 }
-function createimageportion(images){
+
+const createimageportion = (images) => {
     //This function will create HTML images in Bulk 
     //and return the HTML codes
     let htmlportion = "";
@@ -17,15 +18,17 @@ function createimageportion(images){
     return htmlportion;
 
 }
-function contentputter(htmlportion, section_id){
+
+const contentputter = (htmlportion, section_id) => {
     //This function is responsible for putting content
     //in the specific section
     //htmlportion variable is for HTML Codes
     //section_id variable is the container id
-    let Mainsection = document.getElementById(section_id);
+    const Mainsection = document.getElementById(section_id);
     Mainsection.innerHTML += htmlportion;
 }
-function featuredshow(data){
+
+const featuredshow = (data) => {
     //This function is responsible for creating single image HTML Code
     return createimagearray(data.movietitle, data.movieposter)
 
